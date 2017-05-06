cf7030eea69bcd6f437d5d5e6f2f96a1a127a818
sherylee <lkzleexj@163.com>
2017-05-06 14:37:11 +0800

    跟改

--- src/com/tarena/demo/Demo23.java
+++ src/com/tarena/demo/Demo23.java
@@ -1,7 +1,20 @@
 package com.tarena.demo;
 
-public class Demo23 {
-//   10
-//一球从100米高度自由落体，每次落地后反跳回原高度的一半，在落下
-//求它在第10次落地时，共经过多少米？第10次反弹多高？
+public class Demo23
+{
+	//   10
+	//一球从100米高度自由落体，每次落地后反跳回原高度的一半，在落下
+	//求它在第10次落地时，共经过多少米？第10次反弹多高？
+	public static void main(String[] args)
+	{
+		double t = 0;
+		double sum = 100;
+		for (int i = 1; i <= 10; i++)
+		{
+			t = 100 * Math.pow(0.5, i);
+			sum = sum + t;
+		}
+		System.out.println("sum=" + sum);
+		System.out.println("第十次反弹高度：" + t);
+	}
 }
--- src/com/tarena/demo/Demo26.java
+++ src/com/tarena/demo/Demo26.java
@@ -1,8 +1,16 @@
 package com.tarena.demo;
 
-public class Demo26 {
-/*  13
- * 一个整数，它加上100后是一个完全平方数，再加上168又是一个
- * 完全平方数，请问该书是多少？
- */
+public class Demo26
+{
+	/*  13
+	 * 一个整数，它加上100后是一个完全平方数，再加上168又是一个
+	 * 完全平方数，请问该数是多少？
+	 */
+	public static void main(String[] args)
+	{
+		int num;
+		num+100==Math.pow(a, 2);
+		Math.pow(a, 2)+168=Math.pow(b, 2);
+				
+	}
 }
